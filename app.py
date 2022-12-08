87ebf2a6c5dfb307a436522651463a8d0c370ae7
Iwijn <iwijn.voeten@gmail.com>
2022-12-08 18:36:22 +0100

    basic layout with dummy topics

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/topics')
+def topics():
+    topics = ['This is the first topic.',
+                'This is the second topic.',
+                'This is the third topic.',
+                'This is the fourth topic.'
+                ]
+    return render_template('topics.html', topics=topics)
 
 
 if __name__ == '__main__':
